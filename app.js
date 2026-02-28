40e301d36a700373614a3c0efa5a863b0f23049a
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-28 21:06:10 +0430

    new class of food

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ class Animal{
  const cat=new Animal("cat", "white", "2025-3-4","male");
  const dog=new Animal("dog", "blue", "2024-1-23","female");
  const donkey=new Animal("donkey", "green", "2020-11-2","female");
+ class Food{
+    constructor(name, price, taste,color){
+        this.name=name,
+        this.price=price,
+        this.taste=taste,
+         this.color=color
+    }
+ }
+ new Food("rice","180AF","delecoiuce","white");
