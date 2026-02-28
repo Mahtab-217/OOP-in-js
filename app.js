44bf314ffa33c1cf33a42c577603976b955df334
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-28 21:01:02 +0430

    new adding code

---
+++ app.js
@@ -0,0 +1,12 @@
+class Animal{
+    constructor(name,color, age, gender){
+      this.name = name,
+      this.color=color,
+      this.age=age,
+      this.gender= gender
+    }
+    
+}
+ const cat=new Animal("cat", "white", "2025-3-4","male");
+ const dog=new Animal("dog", "blue", "2024-1-23","female");
+ const donkey=new Animal("donkey", "green", "2020-11-2","female");
